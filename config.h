4ccce2daade8f73926feea47da797fad4b8c4361
Jonathan Teran <jonathan.nerat@gmail.com>
2020-08-27 14:54:30 -0300

    apply fuzzymatch patch

--- config.h
+++ config.h
@@ -3,7 +3,8 @@
 
 #include "theme.h"
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int topbar = 1; /* -b  option; if 0, dmenu appears at bottom */
+static int fuzzy  = 1; /* -F  option; if 0, dmenu doesn't use fuzzy matching */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	THEME_FONT,
@@ -11,7 +12,7 @@ static const char *fonts[] = {
 	THEME_EXTRA_FONTS
 #endif
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *prompt      = NULL; /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { THEME_WHITE, THEME_BLACK },
